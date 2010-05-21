aa1c53ff14b301eca7e551891cb9c0c1beb1653b
mattonrails <himself@mattonrails.com>
2010-05-21 18:05:40 -0400

    Start spec'ing

--- app.rb
+++ app.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-	erb :index
+  erb :index
 end
---
+++ spec/captain_hook_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+describe "My App" do
+  include Rack::Test::Methods
+
+  def app
+    @app ||= Sinatra::Application
+  end
+
+  it "should respond to /" do
+    get '/'
+    last_response.should be_ok
+  end
+  
+  
+  
+end
