d48cc20eea8aec1e301b6bd2efa6a7c9e35b7057
mattonrails <himself@mattonrails.com>
2010-07-18 22:00:32 -0400

    Add: placeholders for Twitter username and password

--- lib/twitter_post.rb
+++ lib/twitter_post.rb
@@ -1,7 +1,7 @@
 class TwitterPost
   def initialize(message)
     @message = message
-    httpauth = Twitter::HTTPAuth.new('captainhook_rb', 'dr8FES7e')
+    httpauth = Twitter::HTTPAuth.new('USER_NAME_HERE', 'PASSWORD_HERE')
     @client = Twitter::Base.new(httpauth)
   end
   
