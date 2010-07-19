6023f206f75ef0d04481edac7230458fef2db90f
mattonrails <himself@mattonrails.com>
2010-07-18 21:39:55 -0400

    Add: test JSON, finish specs

--- spec/captain_hook_spec.rb
+++ spec/captain_hook_spec.rb
@@ -1,6 +1,14 @@
-require File.dirname(__FILE__) + '/spec_helper.rb'
+require 'spec_helper'
 
-describe "Captain Hook" do
+$payload = File.open("spec/data/payload.json", "r") do |line|
+  line.gets
+end
+
+$with_post = File.open("spec/data/with_post.json", "r") do |line|
+  line.gets
+end
+
+describe "Commit Receiver" do
   include Rack::Test::Methods
 
   def app
@@ -12,6 +20,24 @@ describe "Captain Hook" do
     last_response.should be_ok
   end
   
+  it "should receive the GitHub payload" do
+    post '/', :payload => $payload
+    last_response.should be_ok
+  end
   
+  it "should parse the JSON" do
+    CommitReceiver.new($payload)
+    @payload.is_a?(Hash) == true
+  end
+  
+  it "should add 'NEW POST' messages to queue" do
+    CommitReceiver.new($with_post)
+    $new_posts.empty? == false
+  end
+  
+  it "should ignore commits that aren't a blog post" do
+    CommitReceiver.new($with_post)
+    $new_posts.empty? == true
+  end
   
 end
