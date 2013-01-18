7435bdf5792b854a70ea6a7e12c6b5d2487a7a3c
Joshua Davison <josh@stringbot.com>
2013-01-18 14:36:25 -0600

    strip trailing whitespace

--- spec/captain_hook_spec.rb
+++ spec/captain_hook_spec.rb
@@ -11,41 +11,41 @@ describe "Captain-Hook" do
     get '/'
     last_response.should be_ok
   end
-  
+
   it "should accept the GitHub payload" do
     APP_CONFIG['token'] = 'secret'
     post '/', :payload => PayloadWithoutPosts, :token => 'secret'
     last_response.should be_ok
   end
-  
+
   describe "without any POST parameters" do
     it "should return a 401 error" do
       post '/'
       last_response.status.should == 401
     end
-    
+
   end
-  
+
   describe "without any new posts" do
     before { @cr = CommitReceiver.new(PayloadWithoutPosts) }
-    
+
     it "should parse the JSON" do
       @payload.is_a?(Hash) == true
     end
-    
+
     it "should ignore the commits" do
       $new_posts.empty? == true
     end
-    
+
   end
-  
+
   describe "with new post(s)" do
     before { @cr = CommitReceiver.new(PayloadWithPost) }
-    
+
     it "should add 'NEW POST' messages to queue" do
       $new_posts.empty? == false
     end
-    
+
   end
-  
+
 end
